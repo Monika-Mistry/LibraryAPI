c1144169d7e3d2edd30738a80d95a4f9d71b9091
Monika-Mistry <monika.mistty@gmail.com>
2021-09-30 21:15:36 +0100

    initial commit

---
+++ src/main/java/org/library/LibraryAPI/LibraryAPIApplication.java
@@ -0,0 +1,12 @@
+package org.library.LibraryAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAPIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAPIApplication.class, args);
+    }
+}
---
+++ src/test/java/org/library/LibraryAPI/LibraryAPIApplicationTest.java
@@ -0,0 +1,14 @@
+package org.library.LibraryAPI;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Ignore
+public class LibraryAPIApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
